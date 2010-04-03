7cd1a36ccc94acff77b68bff681ff640085c87a1
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-04-03 12:07:18 +0300

    Created posts controller

---
+++ app/controllers/posts.rb
@@ -0,0 +1,27 @@
+Jasc.controllers :posts do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :respond_to => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :index do
+  end
+
+  get :show do
+  end
+
+end
