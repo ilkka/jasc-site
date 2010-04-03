ac675da573be660426d05f3b83b5032ccd7c281c
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-04-03 16:21:08 +0300

    Remove useless cruft from post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,14 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
 
 describe Post do
-  before { @post = Post.new }
-  subject { @post }
-
-  it 'can be created' do
-    @post.should_not be_nil
-  end
-
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  
 end
