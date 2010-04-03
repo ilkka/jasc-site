f883cf67053e7bb28fecdd589f4ebb2dcb3aad99
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-04-03 15:00:21 +0300

    Reasonable test for posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,11 +1,29 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
 
 describe "PostsController" do
-  before do
-    get "/"
-  end
+  context "when index route used" do
+    before do
+      @posts = []
+      post = Post.new
+      post.title = "Test post 1"
+      post.body = "Test post 1 body"
+      @posts << post
+      post = Post.new
+      post.title = "Test post 2"
+      post.body = "Test post 2 body"
+      @posts << post
+      Post.expects(:find_all).returns(@posts)
 
-  it "returns hello world" do
-    last_response.body.should == "Hello World"
+      get '/posts'
+    end
+
+    specify { last_response.should be_ok }
+    it "should list all posts" do
+      @posts.each do |post|
+        last_response.body.should include(post.title)
+        last_response.body.should include(post.body)
+      end
+    end
   end
 end
+
