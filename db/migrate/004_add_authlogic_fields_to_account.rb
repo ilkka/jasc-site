ddb277167c4509246a8c364708606400fcaa2a57
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-04-06 22:16:35 +0300

    Migration that adds missing authlogic fields. Maybe still need to rename :salt -> :password_salt

---
+++ db/migrate/004_add_authlogic_fields_to_account.rb
@@ -0,0 +1,25 @@
+class AddAuthlogicFieldsToAccount < ActiveRecord::Migration
+  def self.up
+    change_table :posts do |t|
+      t.string :persistence_token, :null => false
+      t.integer :login_count, :default => 0, :null => false
+      t.datetime :last_request_at
+      t.datetime :last_login_at
+      t.datetime :current_login_at
+      t.string :last_login_ip
+      t.string :current_login_ip
+    end
+  end
+
+  def self.down
+    change_table :posts do |t|
+      t.remove :persistence_token
+      t.remove :login_count
+      t.remove :last_request_at
+      t.remove :last_login_at
+      t.remove :current_login_at
+      t.remove :last_login_ip
+      t.remove :current_login_ip
+    end
+  end
+end
